c3d779f6a16593055c40a138a1b157f87a1b10d9
Kaushal Gupta <kaushalgupta942@gmail.com>
2024-05-05 20:59:28 +0530

    another commit

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -2,20 +2,21 @@ import mongoose, { Mongoose } from "mongoose";
 
 const MONGODB_URL = process.env.MONGODB_URL;
 
-type MongooseConnection = {
+interface MongooseConnection {
   conn: Mongoose | null;
   promise: Promise<Mongoose> | null;
-};
+}
 
 let cached: MongooseConnection = (global as any).mongoose;
 
-if (!cached)
+if (!cached) {
   cached = (global as any).mongoose = {
     conn: null,
     promise: null,
   };
+}
 
-export const connectToDatabase = async function () {
+export const connectToDatabase = async () => {
   if (cached.conn) return cached.conn;
 
   if (!MONGODB_URL) throw new Error("Missing MONGODB_URL");
@@ -28,4 +29,6 @@ export const connectToDatabase = async function () {
     });
 
   cached.conn = await cached.promise;
+
+  return cached.conn;
 };
